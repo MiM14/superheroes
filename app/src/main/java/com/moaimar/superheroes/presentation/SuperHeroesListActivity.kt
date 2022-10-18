3790f8d949df1906054793e57fab61ac07a6a76b
Ismael Martín Moakil <85709107+MiM14@users.noreply.github.com>
2022-10-18 16:51:19 +0200

    First Super hero view

---
+++ app/src/main/java/com/moaimar/superheroes/presentation/SuperHeroesListActivity.kt
@@ -0,0 +1,12 @@
+package com.moaimar.superheroes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moaimar.superheroes.R
+
+class SuperHeroesListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superheroeslist)
+    }
+}
